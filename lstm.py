58fe284d4bb33c5164cc5a85c10a9c71930fc7cc
Tong Wang <wangtong106@gmail.com>
2016-04-16 19:41:55 -0400

    reduced dependency on keras (currently only using init) fixed bug on recurrent output weight matrix - returning all w_*y to be used in d_gate*/d_w fixed bug on cell recurrent not being updated in gradient accumulation loop fixed bug on redundant multiplication of output gate derivative when calculating dy [TODO] refactoring by adding theano functions to return params used in gradient calculation - instead of using one function to return everything

--- lstm.py
+++ lstm.py
@@ -35,7 +35,7 @@ for i in xrange(4):
 	weights.append([])
 	weights[i].append(init(w_shape))
 	weights[i].append(init(v_shape))
-	weights[i].append(K.zeros(b_shape))
+	weights[i].append(T.zeros(b_shape))
 
 # INPUT PERTURBATION FOR NUMERICAL GRADIENTS
 delta = T.matrix('delta', dtype=floatX)
@@ -64,28 +64,29 @@ def _step(x, y, c_tm1, h_tm1):
 		v = weights[i][1]
 		b = weights[i][2]
 
+		# ADD PERTURBATION TO TARGET GATE FOR GRADIENT CALCULATION
 		if d_gate == i:
 			if 0 == d_component:
 				w += delta
 			elif 1 == d_component:
 				v += delta
 
-		gates_in.append(K.dot(w, x) + K.dot(v, h_tm1) + b)
+		gates_in.append(T.dot(w, x) + T.dot(v, h_tm1) + b)
 
 	# CALCULATE GATES
 	gates = []
 	for i in xrange(3):
 		gates.append(inner_activation(gates_in[i]))
-	gates.append(outer_activation(gates_in[3]))
+	gates.append(outer_activation(gates_in[3]))  # [3] is cell, using tanh
 
 	# CALCULATE CELL
-	cell = gates[1] * c_tm1 + gates[0] * gates[3]
+	cell = gates[0] * gates[3] + gates[1] * c_tm1
 
 	# CALCULATE OUTPUT
 	hidden = gates[2] * cell
 
 	# CALCULATE ERROR
-	err = K.dot(hidden, y)  # INNER PRODUCT as LOSS
+	err = T.dot(hidden, y)  # INNER PRODUCT as LOSS
 
 	return cell, hidden, err, theano.gradient.grad(err, gates_in[d_gate]), gates[0], gates[1], gates[2], gates[3]
 
@@ -98,9 +99,9 @@ err = e[-1]
 theano_d = theano.gradient.grad(err, weights[d_gate][d_component])
 
 # COMPILING THEANO FUNCTION
-# returning weights[d_gate][1]: the weight matrix for the recurrent term h_tm1, which is often required by wang gradients
+# returning weights[:][1]: the weight matrix for the recurrent term h_tm1, which is often required by wang gradients
 lstm = theano.function(inputs=[data_in, data_out, c0, h0, delta],
-						outputs=[c, h, e, theano_d, graves_delta, gi, gf, go, gc, weights[d_gate][1]], on_unused_input='ignore')
+						outputs=[c, h, e, theano_d, graves_delta, gi, gf, go, gc, weights[0][1], weights[1][1], weights[2][1], weights[3][1]], on_unused_input='ignore')
 
 # MODEL I/O
 N = 5
@@ -138,8 +139,8 @@ for ir in xrange(params_shape[0]):
 		# delta[ir][ic] = w[ir][ic] * eps
 
 		# y- AND y+
-		[c1, h1, e1, theano_d1, graves_delta1, gi_1, gf_1, go_1, gc_1, v1] = lstm(data_in, data_out, c0, h0, -delta)
-		[c2, h2, e2, theano_d2, graves_delta2, gi_2, gf_2, go_2, gc_2, v2] = lstm(data_in, data_out, c0, h0, delta)
+		[c1, h1, e1, theano_d1, graves_delta1, gi_1, gf_1, go_1, gc_1, wy_i_1, wy_f_1, wy_o_1, wy_c_1] = lstm(data_in, data_out, c0, h0, -delta)
+		[c2, h2, e2, theano_d2, graves_delta2, gi_2, gf_2, go_2, gc_2, wy_i_2, wy_f_2, wy_o_2, wy_c_2] = lstm(data_in, data_out, c0, h0, delta)
 
 		# print('err1', e1)
 		# print('err2', e2)
@@ -170,32 +171,33 @@ for ir in xrange(params_shape[0]):
 		dEdy_t = data_out[-1]  # INNER PRODUCT LOSS:/dy = y_hat (h_size)
 		# print("dEdy_t.shape", dEdy_t.shape)
 
-		def dg_alpha_dw_beta_x(gates, alpha, beta, x_t, w_alpha_y, dy_tm1):
+		def dg_alpha_dw_beta_x(gates, alpha, beta, x_t, wys, dy_tm1):
 			dg_alpha = dg(gates, alpha)
 			if alpha != beta:
 				x_t = np.zeros_like(x_t)
-			return dg_alpha.reshape(h_size, 1) * (x_t + np.array([np.dot(w_alpha_y, tmp) for tmp in dy_tm1]))
+			return dg_alpha.reshape(h_size, 1) * (x_t + np.array([np.dot(wys[alpha], tmp) for tmp in dy_tm1]))
 
-		def dc_dw_beta_x(gates, c_tm1, dc_tm1, beta, x_t, w_alpha_y, dy_tm1):
-			retval = gates[0].reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 3, beta, x_t, w_alpha_y, dy_tm1)
-			retval += gates[3].reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 0, beta, x_t, w_alpha_y, dy_tm1)
+		def dc_dw_beta_x(gates, c_tm1, dc_tm1, beta, x_t, wys, dy_tm1):
+			retval = gates[0].reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 3, beta, x_t, wys, dy_tm1)
+			retval += gates[3].reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 0, beta, x_t, wys, dy_tm1)
 			retval += gates[1].reshape(h_size, 1) * dc_tm1
-			retval += c_tm1.reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 1, beta, x_t, w_alpha_y, dy_tm1)
+			retval += c_tm1.reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 1, beta, x_t, wys, dy_tm1)
 			return retval
 
-		def dy_dw_beta_x(gates, beta, cell, x_t, w_alpha_y, dy_tm1, dc):
-			retval = cell * dg(gates, 2)
-			retval = retval.reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 2, beta, x_t, w_alpha_y, dy_tm1)
-			retval = retval + gates[2].reshape(h_size, 1) * dc
+		def dy_dw_beta_x(gates, beta, cell, x_t, wys, dy_tm1, dc):
+			retval = cell.reshape(h_size, 1) * dg_alpha_dw_beta_x(gates, 2, beta, x_t, wys, dy_tm1)
+			retval += gates[2].reshape(h_size, 1) * dc
 			return retval
 
 		dy = np.zeros((h_size, params_shape[0], params_shape[1])).astype(floatX)
 		dc = np.zeros((h_size, params_shape[0], params_shape[1])).astype(floatX)
 		c_tm1 = c0
+		wys = [wy_i_1, wy_f_1, wy_o_1, wy_c_1]
 		for t in xrange(0, N):
+			c_tm1 = c1[t - 1] if t > 0 else c0
 			gates = [gi_1[t], gf_1[t], go_1[t], gc_1[t]]
-			dc = dc_dw_beta_x(gates, c_tm1, dc, d_gate, data_in[t], v1, dy)
-			dy = dy_dw_beta_x(gates, d_gate, c1[t], data_in[t], v1, dy, dc)
+			dc = dc_dw_beta_x(gates, c_tm1, dc, d_gate, data_in[t], wys, dy)
+			dy = dy_dw_beta_x(gates, d_gate, c1[t], data_in[t], wys, dy, dc)
 
 		dy = dEdy_t.reshape(h_size, 1) * dy
 		print("wang:\t%.6e" % (dy[0][ir][ic]))
